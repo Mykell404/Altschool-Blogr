f52d15ef0484f81675e37e53bbbc45ac472da477
Mykell404 <milebamicheal@gmail.com>
2022-11-03 10:53:23 +0100

    added styles and some minor performance fix

--- blog/__init__.py
+++ blog/__init__.py
@@ -2,6 +2,7 @@ import os
 
 from . import db
 from . import auth
+from . import blog
 from flask import Flask
 
 
@@ -14,6 +15,8 @@ def create_app(test_config=None):
     )
     db.init_app(app)
     app.register_blueprint(auth.bp)
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
--- blog/auth.py
+++ blog/auth.py
@@ -1,3 +1,4 @@
+import email
 import functools
 
 from flask import (
@@ -13,8 +14,11 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
         username = request.form['username']
         password = request.form['password']
+        email = request.form['email']
         db = get_db()
         error = None
 
@@ -22,12 +26,19 @@ def register():
             error = 'Username is required.'
         elif not password:
             error = 'Password is required.'
+        elif not firstname:
+            error = 'First name is required'
+        elif not lastname:
+            error = 'Last name is required'
+        elif not email:
+            error = 'Email is required'
 
         if error is None:
             try:
                 db.execute(
-                    "INSERT INTO user (username, password) VALUES (?, ?)",
-                    (username, generate_password_hash(password)),
+                    "INSERT INTO user (username, password, firstname, lastname, email) VALUES (?, ?, ?, ?, ?)",
+                    (username, generate_password_hash(
+                        password), firstname, lastname, email),
                 )
                 db.commit()
             except db.IntegrityError:
@@ -48,7 +59,8 @@ def login():
         db = get_db()
         error = None
         user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username,)
+            'SELECT * FROM user WHERE username = ? OR email = ?', (username,
+                                                                   username)
         ).fetchone()
 
         if user is None:
